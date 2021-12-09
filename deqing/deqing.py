e649b995da60474b03ca318f73d434183a7dfedc
jerry-1121 <78292611+jerry-1121@users.noreply.github.com>
2021-12-10 00:02:50 +0800

    Update deqing.py

--- deqing/deqing.py
+++ deqing/deqing.py
@@ -14,7 +14,7 @@ while True:
     url3 = 'http://wechat.9client.com'
  #ping 这个域名是否可以连通, -w 最大超时时间超出后退出报异常 -c 试探域名连通性的次数 -i 每一次试探的时间>间隔.
 #   url2 = 'ping -w 6 -c 5 -i 3 wechat.9client.com'
-    err = """curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=62086a86-4b8b-457f-8428-865e9a56bc99\' -H \'Content-Type: application/json\' -d \'{"msgtype": "text","text": {"content": "德勤服务进程出现异常情况\n请引起注意,请及时排出故障"} }'"""
+    err = """curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=62086a86-4b8b-457f-8428-865e\' -H \'Content-Type: application/json\' -d \'{"msgtype": "text","text": {"content": "德勤服务进程出现异常情况\n请引起注意,请及时排出故障"} }'"""
     http = os.system(url + ' >/dev/null')
     #requestse = requests.get(url3)
     #httpt = requestse.ok         #获取请求响应的布尔值   
